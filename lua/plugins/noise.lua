c4303324575801101194221b49602b8e3b8a9a95
Pradeek <pradeek.k@gmail.com>
2023-12-23 09:22:16 +0530

    added lualine

---
+++ lua/plugins/noise.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {},
+  dependencies = {
+       "MunifTanjim/nui.nvim",
+       "rcarriga/nvim-notify",
+      config = function()
+      require("notify").setup({
+        background_color = "#000000"
+      })
+    end,
+    }
+}
